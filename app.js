1c3c55177cf660343bf10cce3b47de34829f9611
kafsalon <jfglamport@gmail.com>
2018-06-18 14:39:04 +0400

    passage des variables par ajax

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+
 
 const port = 7777;
 
 app.use("/static", express.static('static'));
-
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
 /**
  * Route vers index.html 
  */
@@ -12,12 +15,18 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+
 /**
- * Route vers le form 
+ * Route vers index.html 
  */
 app.post('/add', function (req, res) {
-    res.end();
+    const num_1 = Number(req.body.num1);
+    const num_2 = Number(req.body.num2);
+    const add = num_1 + num_2;
+   // res.send(`${add}`);
+    res.send(add+"");
 });
+
 
 /**
  * Port d'écoute du serveur
--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,23 @@
 $(function () {
     $('#btn').on('click', function (e) {
         e.preventDefault();
+        const nombre1 = $('#nb1').val();
+        const nombre2 = $('#nb2').val();
+        // $.post( "http://localhost:7777/add",{nb1:nombre1,nb2:nombre2}, function( d ) {
+        //     $('#resultat').html("<div class='lead text-center'>Résultat</div> <b class='display-2'>" + d +"</b>");
+        //   });
         $.ajax({
-            type: "POST",
-            data: "nb1=" + nb1 + "&nb2=" + nb2,
+            type: 'POST',
+            data: {
+                num1: nombre1,
+                num2: nombre2
+            },
             url: "http://localhost:7777/add",
             success: function (d) {
-                const nombre1 = nb1.value;
-                const nombre2 = nb2.value;
-                const resultat = Number(nombre1) + Number(nombre2);
-                $('#resultat').html("<div class='lead text-center'>Résultat</div> <b class='display-2'>" + resultat+"</b>");
+                $('#resultat').html("<div class='lead text-center'>Résultat</div> <b class='display-2'>" + d + "</b>");
             },
             error: function (e) {
-                console.log("erreur :");
+                console.log("erreur :", e);
             }
         });
     });
